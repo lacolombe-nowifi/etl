06fea3fec8702d7b01049a2439ae32693456a738
ncho-sqd <cjw.neil@gmail.com>
2018-10-08 00:28:58 -0500

    Preliminary continuous divvy RSS scrape using dict comparison.

---
+++ scrape_divvy_rss_feed.py
@@ -0,0 +1,44 @@
+import csv
+import requests
+import time
+
+URL = 'https://feeds.divvybikes.com/stations/stations.json'
+INTERVAL = 30   # in seconds
+
+def stream_dict_save(file, dict_lst):
+    with open(file, "a") as f:
+        keys = dict_lst[0].keys()
+        dict_writer = csv.DictWriter(f, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(dict_lst)
+
+def comp_dict_const(dict_lst):
+    return({e['id']: e['availableBikes'] for e in dict_lst})
+
+def comp_dict(dict_base, dict_comp):
+    return([k for k, v in enumerate(dict_comp.items()) 
+            if v[1] != dict_base.get(v[0]) 
+            or dict_base.get(v[0]) is None])
+
+if __name__ == '__main__':
+    while True:
+        req = requests.get(URL).json()['stationBeanList']
+
+        try:
+            prev
+            prev_comp = comp_dict_const(prev)
+            req_comp = comp_dict_const(req)
+            update_idx = comp_dict(prev_comp, req_comp)
+            
+            if update_idx:
+                update_val = [v for k, v in enumerate(req) if k in update_idx]
+                stream_dict_save("test.csv", update_val)
+            prev = req
+            print("update: {}".format(len(update_idx)))
+
+        except:
+            stream_dict_save("test.csv", req)
+            prev = req
+            print("initial")
+        
+        time.sleep(INTERVAL)
